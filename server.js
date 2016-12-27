5f69914126c903e0f7aeb35201f12447059de7d8
Calivantage <nicolaus.lawson@gmail.com>
2016-12-26 21:17:37 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = require('./middleware.js');
+
+// app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication,function(req, res) {
+  res.send('learn About Us today');
+});
+
+app.use(express.static(__dirname + '/public'));
+// console.log(__dirname);
+
+app.listen(port, function() {
+  console.log('Express server started on ' + port + '!');
+});
